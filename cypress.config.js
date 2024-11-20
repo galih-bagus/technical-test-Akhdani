b28aa7769915c4a79aa4674b5381d4bface58938
galih bagus <47969692+galih-bagus@users.noreply.github.com>
2024-11-20 15:43:51 +0700

    fix : update config

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,35 @@
 const { defineConfig } = require("cypress");
+const browserify = require("@cypress/browserify-preprocessor");
+const dotenv = require("dotenv");
+// import dotenv from "dotenv";
+dotenv.config();
 
 module.exports = defineConfig({
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  video: false,
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 20000,
+  env: {
+    baseUrl: process.env.BASE_URL,
+    // username: process.env.USERNAMESUPERADMIN,
+    // password: process.env.PASSWORDSUPERADMIN,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      let options = browserify.defaultOptions;
+      options.browserifyOptions.transform[1][1].plugins.push([
+        "module-resolver",
+        {
+          alias: {
+            "@tests": "./tests",
+            "@helpers": "./tests/helpers",
+          },
+        },
+      ]);
+      on("file:preprocessor", browserify(options));
     },
+    specPattern: "tests/scenarios/**/*.test.js",
   },
 });
