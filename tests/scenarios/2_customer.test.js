3c3885e14677754d1cf326cb0bee7e79c359b343
galih bagus <47969692+galih-bagus@users.noreply.github.com>
2024-11-21 20:59:53 +0700

    add : [TC0023] Can deposit with negative number

--- tests/scenarios/2_customer.test.js
+++ tests/scenarios/2_customer.test.js
@@ -26,7 +26,7 @@ describe("Test Case Customer", () => {
       });
    });
    describe("Deposit", () => {
-      it("[TC0021] Can deposit with valid data", () => {
+      it.skip("[TC0021] Can deposit with valid data", () => {
          /* 
         1. Click button "Customer Login"
         2. Choose customer
@@ -41,6 +41,22 @@ describe("Test Case Customer", () => {
          element.fillFilledXpath(customerPage.inputAmount, 10000);
          element.clickXpath(customerPage.buttonDepositAction);
          assert.shouldContainTextXpath(customerPage.depositMessage, "Deposit Successful");
+      });
+      it("[TC0023] Can deposit with negative number", () => {
+         /* 
+        1. Click button deposit
+        2. Fill field deposit with negative number
+        3. Click button "Deposit"
+         */
+         element.clickXpath(customerPage.buttonCustomerLoginPage);
+         assert.shouldBeVisibleXpath(customerPage.assertionLoginCustomerPage);
+         element.fillSelectXpath(customerPage.selectCustomerLogin, 1);
+         element.clickXpath(customerPage.buttonCustomerLogin);
+         assert.shouldBeVisibleXpath(customerPage.assertionLoginCustomer);
+         element.clickXpath(customerPage.buttonDeposit);
+         element.fillFilledXpath(customerPage.inputAmount, -10000);
+         element.clickXpath(customerPage.buttonDepositAction);
+         assert.shouldContainTextXpath(customerPage.depositMessage, "Deposit Failed, Please input valid amount");
       });
    });
 });
