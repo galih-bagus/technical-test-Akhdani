d3196b7f6902dacb580a7e612ee7d4bd06debe86
galih bagus <47969692+galih-bagus@users.noreply.github.com>
2024-11-21 20:29:43 +0700

    add : scenario customer login

---
+++ tests/scenarios/2_customer.test.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+import * as element from "@helpers/element";
+import * as route from "@helpers/route";
+import { ROUTES } from "@tests/const/routes";
+import * as customerPage from "@tests/pages/customer.page";
+import * as assert from "@helpers/assert";
+import * as userData from "@tests/data/user.data";
+
+beforeEach(() => {
+   route.visit(ROUTES.login);
+});
+describe("Test Case Customer", () => {
+   describe("Login Customer", () => {
+      it("[TC0002] Login as Customer", () => {
+         /* 
+        1. Click button "Customer Login"
+        2. Choose customer
+        3. Click button "Login"
+         */
+         element.clickXpath(customerPage.buttonCustomerLoginPage);
+         assert.shouldBeVisibleXpath(customerPage.assertionLoginCustomerPage);
+         element.fillSelectXpath(customerPage.selectCustomerLogin, 1);
+         element.clickXpath(customerPage.buttonCustomerLogin);
+         assert.shouldBeVisibleXpath(customerPage.assertionLoginCustomer);
+      });
+   });
+});
