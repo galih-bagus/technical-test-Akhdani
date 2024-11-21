cfe0b112d03b2ab906bee629d2c48c6f890e2c7a
galih bagus <47969692+galih-bagus@users.noreply.github.com>
2024-11-21 20:02:55 +0700

    add : role bank manager page

---
+++ tests/pages/bankManager.page.js
@@ -0,0 +1,10 @@
+export const buttonBankManagerLogin = `//button[contains(text(),'Bank Manager Login')]`;
+export const buttonAddCustomerPage = `//button[contains(text(),'Add Customer')]`;
+export const inputFirstName = `//input[@ng-model='fName']`;
+export const inputLastName = `//input[@ng-model='lName']`;
+export const inputPostalCode = `//input[@ng-model='postCd']`;
+export const buttonAddCustomer = `//button[@type='submit' and contains(text(),'Add Customer')]`;
+export const buttonAddOpenAccountPage = `//button[contains(text(),'Open Account')]`;
+export const selectFieldCustomer = `//select[@id='userSelect']`;
+export const selectFieldCurrency = `//select[@id='currency']`;
+export const buttonAddOpenAccount = `//button[contains(text(),'Process')]`;
