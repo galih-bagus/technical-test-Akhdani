0a28a9cc197dafa77b3d700a1bfcc46a8dbfb543
galih bagus <47969692+galih-bagus@users.noreply.github.com>
2024-11-21 20:29:34 +0700

    add : customer page login

---
+++ tests/pages/customer.page.js
@@ -0,0 +1,5 @@
+export const buttonCustomerLoginPage = `//button[contains(text(),'Customer Login')]`;
+export const assertionLoginCustomerPage = `//label[contains(text(),'Your Name')]`;
+export const buttonCustomerLogin = `//button[contains(text(),'Login')]`;
+export const selectCustomerLogin = `//select[@ng-model='custId']`;
+export const assertionLoginCustomer = `//span[contains(text(),'Hermoine')]`;
