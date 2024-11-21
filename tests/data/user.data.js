8e5b0543088992c67c0b9c47ab1d10af3e40c69c
galih bagus <47969692+galih-bagus@users.noreply.github.com>
2024-11-21 20:02:21 +0700

    add : customer data

--- tests/data/user.data.js
+++ tests/data/user.data.js
@@ -1,10 +1,22 @@
 import * as helperFunction from "@helpers/function";
 export const INVALID_REGISTER = {
-  username: "asd",
-  password: "1234",
+   username: "asd",
+   password: "1234"
 };
 
 export const VALID_REGISTER = {
-  username: "user"+helperFunction.getDateTime(),
-  password: "password",
+   username: "user" + helperFunction.getDateTime(),
+   password: "password"
+};
+
+export const VALID_CUSTOMER = {
+   firstName: "user.galih",
+   lastName: helperFunction.getDateTime(),
+   postalCode: helperFunction.getDateTime()
+};
+
+export const DUPLICATE_CUSTOMER = {
+   firstName: "Hermoine",
+   lastName: "Granger",
+   postalCode: "E859AB"
 };
