feb67ce9ca1911f697db44a9a29eff8f7b7330e8
galih bagus <47969692+galih-bagus@users.noreply.github.com>
2024-11-21 12:27:19 +0700

    add : element login page

---
+++ tests/pages/login.page.js
@@ -0,0 +1,6 @@
+export const buttonLogin = "//a[contains(text(),'Log in')]";
+export const assertPopUp = "//h5[contains(text(),'Log in')]";
+export const usernameField = "//input[@id='loginusername']";
+export const passwordField = "//input[@id='loginpassword']";
+export const buttonLoginUser = "//button[normalize-space()='Log in']";
+export const assertLogin = "//a[@id='nameofuser']";
