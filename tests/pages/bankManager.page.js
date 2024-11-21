7767d83a3c5deaf965f2180ee1f68ed272f136e7
galih bagus <47969692+galih-bagus@users.noreply.github.com>
2024-11-21 20:21:28 +0700

    add : customer page in bank manager

--- tests/pages/bankManager.page.js
+++ tests/pages/bankManager.page.js
@@ -8,3 +8,6 @@ export const buttonAddOpenAccountPage = `//button[contains(text(),'Open Account'
 export const selectFieldCustomer = `//select[@id='userSelect']`;
 export const selectFieldCurrency = `//select[@id='currency']`;
 export const buttonAddOpenAccount = `//button[contains(text(),'Process')]`;
+export const buttonCustomerPage = `//button[contains(text(),'Customers')]`;
+export const inputSearchCustomer = `//input[@ng-model='searchCustomer']`;
+export const customerFirstName = `//tbody//tr[1]//td[1]`;
