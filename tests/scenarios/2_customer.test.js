41ece7912ac02629c37337a09e9dac0390d4677e
galih bagus <47969692+galih-bagus@users.noreply.github.com>
2024-11-21 21:13:17 +0700

    add : scenario [TC0024] Can deposit with amount more than 1000000000000000000000000000000000000000000

--- tests/scenarios/2_customer.test.js
+++ tests/scenarios/2_customer.test.js
@@ -12,7 +12,7 @@ beforeEach(() => {
 });
 describe("Test Case Customer", () => {
    describe("Login Customer", () => {
-      it.skip("[TC0002] Login as Customer", () => {
+      it("[TC0002] Login as Customer", () => {
          /* 
         1. Click button "Customer Login"
         2. Choose customer
@@ -26,7 +26,7 @@ describe("Test Case Customer", () => {
       });
    });
    describe("Deposit", () => {
-      it.skip("[TC0021] Can deposit with valid data", () => {
+      it("[TC0021] Can deposit with valid data", () => {
          /* 
         1. Click button "Customer Login"
         2. Choose customer
@@ -57,6 +57,29 @@ describe("Test Case Customer", () => {
          element.fillFilledXpath(customerPage.inputAmount, -10000);
          element.clickXpath(customerPage.buttonDepositAction);
          assert.shouldContainTextXpath(customerPage.depositMessage, "Deposit Failed, Please input valid amount");
+      });
+      it("[TC0024] Can deposit with amount more than 1000000000000000000000000000000000000000000", () => {
+         /* 
+        1. Click button deposit
+        2. Fill field deposit with amount more than 1000000000000000000000000000000000000000000
+        3. Click button "Deposit"
+         */
+         element.clickXpath(customerPage.buttonCustomerLoginPage);
+         assert.shouldBeVisibleXpath(customerPage.assertionLoginCustomerPage);
+         element.fillSelectXpath(customerPage.selectCustomerLogin, 1);
+         element.clickXpath(customerPage.buttonCustomerLogin);
+         assert.shouldBeVisibleXpath(customerPage.assertionLoginCustomer);
+         element.clickXpath(customerPage.buttonDeposit);
+         element.fillFilledXpath(customerPage.inputAmount, 1000000000000000000000000000000000000000000);
+         element.clickXpath(customerPage.buttonDepositAction);
+         assert.shouldContainTextXpath(customerPage.depositMessage, "Deposit Successful");
+         assert.shouldContainTextXpath(
+            customerPage.amountBalance(parseFloat(1000000000000000000000000000000000000000000) + parseFloat(5096)),
+            Math.round(parseFloat(1000000000000000000000000000000000000000000) + parseFloat(5096)).toLocaleString(
+               "fullwide",
+               { useGrouping: false }
+            )
+         );
       });
    });
 });
