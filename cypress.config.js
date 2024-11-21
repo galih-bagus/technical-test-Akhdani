82a183aa771a377b8a2c352a8673897ae00490aa
galih bagus <47969692+galih-bagus@users.noreply.github.com>
2024-11-21 12:27:03 +0700

    fix : add username and password from .env on config

--- cypress.config.js
+++ cypress.config.js
@@ -5,31 +5,31 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = defineConfig({
-  viewportWidth: 1920,
-  viewportHeight: 1080,
-  video: false,
-  chromeWebSecurity: false,
-  defaultCommandTimeout: 20000,
-  env: {
-    baseUrl: process.env.BASE_URL,
-    // username: process.env.USERNAMESUPERADMIN,
-    // password: process.env.PASSWORDSUPERADMIN,
-  },
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      let options = browserify.defaultOptions;
-      options.browserifyOptions.transform[1][1].plugins.push([
-        "module-resolver",
-        {
-          alias: {
-            "@tests": "./tests",
-            "@helpers": "./tests/helpers",
-          },
-        },
-      ]);
-      on("file:preprocessor", browserify(options));
-    },
-    specPattern: "tests/scenarios/**/*.test.js",
-  },
+   viewportWidth: 1920,
+   viewportHeight: 1080,
+   video: false,
+   chromeWebSecurity: false,
+   defaultCommandTimeout: 20000,
+   env: {
+      baseUrl: process.env.BASE_URL,
+      username: process.env.USERNAMEVALID,
+      password: process.env.PASSWORDVALID
+   },
+   e2e: {
+      setupNodeEvents(on, config) {
+         // implement node event listeners here
+         let options = browserify.defaultOptions;
+         options.browserifyOptions.transform[1][1].plugins.push([
+            "module-resolver",
+            {
+               alias: {
+                  "@tests": "./tests",
+                  "@helpers": "./tests/helpers"
+               }
+            }
+         ]);
+         on("file:preprocessor", browserify(options));
+      },
+      specPattern: "tests/scenarios/**/*.test.js"
+   }
 });
